e3028d3e87b005abee3db9795e7f9d7a34ef2ac2
Chama <chama.em14@gmail.com>
2020-10-16 15:16:42 +0200

    implémentation du add montage et add tache

--- src/model/Tache.py
+++ src/model/Tache.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from src.model.Etape import Etape
+
 
 class Tache:
     """Une tâche à acomplire"""
@@ -12,4 +14,18 @@ class Tache:
     def __init__(self, points: int):
         self.points = points
         self.etapes = []
-        
+
+    def add_etape(self, etape: Etape):
+        """Ajoute les étapes dans la tache
+
+        Liste ordonnée des étapes
+
+        :param etape: la nouvelle étape à ajouter
+        :type : Etape
+        """
+        assert etape is not None
+        for e in self.etapes :
+            assert e != etape
+
+        self.etapes.append(etape)
+        return self
