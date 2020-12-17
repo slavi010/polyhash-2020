283ce099d324a1e0a47d83e3016c7779f457507b
Slavi <pro@slavi.dev>
2020-12-17 08:48:42 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Polyhash-2020 Solver',
+      version='1.0',
+      description='Trouver et retourne des solutions pour le polyhash 2020',
+      author='Sviatoslav BESNARD, Chama EL MAJENA, Aurelie BOULAIS, Rémis BORIUS',
+      author_email='etudiant@slavi.dev',
+     )
